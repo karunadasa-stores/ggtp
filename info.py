d072aca49e11d5b3e59e2dff028556752655316d
karunadasa-stores <104718505+karunadasa-stores@users.noreply.github.com>
2022-05-03 09:43:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 bot_owner = 'https://t.me/Gavindu_Tharaka'
 bot_url = 'https://t.me/sinhalasubdown_bot'
 bot_username = "sinhalasubdown_bot"
-database_channel = [-1001219285402, -1001682159273]
+database_channel = [-100589735155, -100781583347]
 main_channel = -1001590852342
 admin_ids = [1376213565]
 
